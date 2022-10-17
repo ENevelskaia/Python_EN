0fbba30026dd75c7806fc2d6cc266e2a89c14acc
Ekaterina Nevelskaia <apple666@mail.ru>
2022-10-18 00:41:04 +0300

    Add_3

--- 1.py
+++ 1.py
@@ -6,3 +6,13 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+    def _avg_grade(self):
